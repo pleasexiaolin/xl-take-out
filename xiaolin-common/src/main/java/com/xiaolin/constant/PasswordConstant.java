58280293d27470c30c595ca8bfc3a3a1ad1eba4d
xiaolin <pleasexiaolin@gmail.com>
2025-11-24 01:16:46 +0800

    info：項目初始化 user：小林 date：2025/11/24

---
+++ xiaolin-common/src/main/java/com/xiaolin/constant/PasswordConstant.java
@@ -0,0 +1,10 @@
+package com.xiaolin.constant;
+
+/**
+ * 密码常量
+ */
+public class PasswordConstant {
+
+    public static final String DEFAULT_PASSWORD = "123456";
+
+}
---
+++ xiaolin-common/src/main/java/com/xiaolin/exception/ShoppingCartBusinessException.java
@@ -0,0 +1,9 @@
+package com.xiaolin.exception;
+
+public class ShoppingCartBusinessException extends BaseException {
+
+    public ShoppingCartBusinessException(String msg) {
+        super(msg);
+    }
+
+}
